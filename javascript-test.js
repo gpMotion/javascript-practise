075b50d5261c35d9eda3f7b55fedf5e3a8ac7859
Gavin Pinkney <gav.pinkney@gmail.com>
2020-05-29 16:03:01 +0900

    finished the challenge

--- javascript-test.js
+++ javascript-test.js
@@ -5,3 +5,32 @@ admin = 'John'
 name = admin
 
 console.log(name)
+
+let question1;
+question1 = 23 + 97;
+console.log(question1);
+
+let question2;
+question2 = 4 + 7 + 9 + 15 + 2.2 + 10;
+console.log(question2)
+
+let question3;
+question3 = (4 + 6 + 9) / 77
+console.log(question3)
+
+let a = 10;
+console.log(a);
+
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+let max;
+max = 57;
+let actual;
+actual = max - 13;
+let percentage
+percentage = actual / max
+
+console.log(percentage)
