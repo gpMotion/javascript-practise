2e620eda5837b6c8f693afc370118f9edbba2658
Gavin Pinkney <gav.pinkney@gmail.com>
2020-06-01 13:14:14 +0900

    string practise added

---
+++ stringsinjava.js
@@ -0,0 +1,16 @@
+let string = 'The revolution will not be televised'
+string;
+
+console.log(string)
+
+let badString = string;
+badString;
+
+console.log(badString)
+
+let one = 'Hello, ';
+let two = 'how are you?';
+let joined = one + two;
+joined;
+
+console.log(joined)
